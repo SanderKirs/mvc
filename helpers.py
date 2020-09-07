3769b539decf42bcab8d7b2761f1c7b5cdfcd133
Sander Kirs <sander.kirs@khk.ee>
2020-09-07 15:41:21 +0300

    On loodud abi funktsionaalsus - elementide lisamine

---
+++ helpers.py
@@ -0,0 +1,10 @@
+elemendid = []
+
+#Lisame ELEMENDI juurde
+def lisa_element(nimetus, hind, kogus):
+    elemendid.append({"nimetus":nimetus, "hind":hind, "kogus":kogus})
+
+
+#Lisame ELEMENDID KORRAGA juurde
+def lisa_elemendid(elementide_nimekiri):
+    elemendid = elementide_nimekiri
