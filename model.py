43b69925a0348fd35e6bda5a6db25f13e5f62eee
Sander Kirs <sander.kirs@khk.ee>
2020-09-09 11:42:28 +0300

    On lisatud kõik kirjutatud funktioonid

---
+++ model.py
@@ -0,0 +1,25 @@
+
+class Model:
+    def __init__(self, elemendid):
+        self.lisa_elemendid(andmed)
+#elementide lisamine
+    def lisa_elemendid(self, elemendid):
+        helpers.lisa_elemendid(elemendid)
+#elemendi lisamine
+    def lisa_element(self, nimetus, hind, kogus):
+        helpers.lisa_element(elemendid)
+#elemendi lugemine
+    def loe_element(self,nimetus):
+        helpers.loe_element(elemendid)
+#elementide lugemine
+    def loe_elemendid(self):
+        helpers.loe_elemendid(elemendid)
+#elemendi kustutamine
+    def kustuta_element(self,nimetus):
+        helpers.kustuta_element(elemendid)
+#elementide kustutamine
+    def kustuta_elemendid(self):
+        helpers.kustuta_elemendid(elemendid)
+#elemendi uuendamine
+    def uuenda_element(self,nimetus, hind, kogus):
+        helpers.uuenda_element(elemendid)
